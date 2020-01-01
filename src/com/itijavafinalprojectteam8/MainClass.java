272a67dfe3cfd9463414584b0847230c333cf9a8
Atef Hares <atefhares1@gmail.com>
2020-01-01 13:43:40 +0200

    initial project files

---
+++ src/com/itijavafinalprojectteam8/MainClass.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.itijavafinalprojectteam8;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author ahares
+ */
+public class MainClass extends Application{
+    public static void main(String[] args) {
+        System.out.println("This is the client!");
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
