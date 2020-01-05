6d35d687fe01782272b0c2518000d8c1d8cebf82
MohamedAbouserei <medoaboserii@gmail.com>
2020-01-05 21:57:34 +0200

    Player Controller With almost Full Functionality

--- src/playercontroller/PlayerController.java
+++ src/playercontroller/PlayerController.java
@@ -12,47 +12,258 @@ import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-/**asd
+/**
  *
  * @author moham
  */
 public class PlayerController {
-      public boolean SignUpJSON(String Name,String Email,String Password) {
-	DataInputStream is;
-	DataOutputStream os;
-	boolean result = true;
-	String noReset = "Could not reset.";
-	String reset = "The server has been reset.";
+    private static Socket SOCKET = null;
+    
+    private static final String NORESET = "Could not reset.";
+    private static final String RESET = "The server has been reset.";
+    private static  DataInputStream GENRAL_IS;
+    private static  DataOutputStream GENRAL_OS;
+    private static JSONObject RESPONSE;
+    private static JSONArray Data_Array;
+    public  static Boolean SignIn_Status=false;
+    public  static Boolean SignUp_Status=false;
+    
+    
+    /*======================================================================================================*/
+    private String Player_Email;
+    private String Player_Name;
+    private int Player_Points;
+
+    public String getPlayer_Email() {
+        return Player_Email;
+    }
+
+    public void setPlayer_Email(String Player_Email) {
+        this.Player_Email = Player_Email;
+    }
+
+    public String getPlayer_Name() {
+        return Player_Name;
+    }
+
+    public void setPlayer_Name(String Player_Name) {
+        this.Player_Name = Player_Name;
+    }
+
+    public int getPlayer_Points() {
+        return Player_Points;
+    }
+
+    public void setPlayer_Points(int Player_Points) {
+        this.Player_Points = Player_Points;
+    }
+    /*======================================================================================================*/
+    public PlayerController()
+    {
+        try {
+                SOCKET = new Socket(InetAddress.getByName("127.0.0.1"), 5006);
+                GENRAL_IS = new DataInputStream(SOCKET.getInputStream());
+		GENRAL_OS = new DataOutputStream(SOCKET.getOutputStream());
+                Get_Response_JSON();
+                //System.err.println("1");
+            } catch (IOException ex) {
+                Logger.getLogger(PlayerController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+         
+
+    }
+      public boolean SignUp_JSON(String Name,String Email,String Password) {
+	
+	boolean Result = true;
+	
 		
 	try {
-		Socket socket = new Socket(InetAddress.getByName("127.0.0.1"), 5006);
 		String string = "{ \"requestStatus\": { \"statusName\": \"SignUp\", }, \"Data\": [ { \"Name\": \""+Name+"\", \"Email\": \""+Email+"\", \"Password\": \""+Password+"\", \"Status\": \"1\", } ] }";
-		is = new DataInputStream(socket.getInputStream());
-		os = new DataOutputStream(socket.getOutputStream());
-		PrintWriter pw = new PrintWriter(os);
+		
+		PrintWriter pw = new PrintWriter(GENRAL_OS);
 		pw.println(string);
 		pw.flush();
-		is.close();
-		os.close();
+		GENRAL_IS.close();
+		GENRAL_OS.close();
 			
 	} catch (IOException e) {
-		result = false;
-		System.out.println(noReset);
+		Result = false;
+		System.out.println(NORESET);
 		e.printStackTrace();			
 	} catch (Exception e) {
-		result = false;
-		System.out.println(noReset);
+		Result = false;
+		System.out.println(NORESET);
 		e.printStackTrace();
 	}
-	System.out.println(reset);
-	return result;
+	System.out.println(RESET);
+	return Result;
 }
+       public boolean SignIn_JSON(String Email,String Password) {
+	
+	boolean Result = true;
+	
+		
+	try {
+		String string = "{ \"requestStatus\": { \"statusName\": \"SignIn\", }, \"Data\": [ {\"Email\": \""+Email+"\", \"Password\": \""+Password+"\", \"Status\": \"1\", } ] }";
+		
+		PrintWriter pw = new PrintWriter(GENRAL_OS);
+		pw.println(string);
+		pw.flush();
+		
+			
+	} catch (Exception e) {
+		Result = false;
+		System.out.println(NORESET);
+		e.printStackTrace();
+	}
+	System.out.println(RESET);
+	return Result;
+}
+public boolean Get_Users_JSON() {
+	
+	boolean Result = true;
+	
+		
+	try {
+		String string = "{ \"requestStatus\": { \"statusName\": \"GetUsers\"} }";
+		
+		PrintWriter pw = new PrintWriter(GENRAL_OS);
+		pw.println(string);
+		pw.flush();
+		
+			
+	} catch (Exception e) {
+		Result = false;
+		System.out.println(NORESET);
+		e.printStackTrace();
+	}
+	System.out.println(RESET);
+	return Result;
+}
+public boolean Send_GameRequest_JSON(String Email_1,String Email_2) {
+	
+	boolean Result = true;
+	
+		
+	try {
+		String string = "{ \"requestStatus\": { \"statusName\": \"GameRequest\", }, \"Data\": [ {\"Email_1\": \""+Email_1+"\", \"Email_2\": \""+Email_2+"\"} ] }";
+		
+		PrintWriter pw = new PrintWriter(GENRAL_OS);
+		pw.println(string);
+		pw.flush();
+		
+			
+	} catch (Exception e) {
+		Result = false;
+		System.out.println(NORESET);
+		e.printStackTrace();
+	}
+	System.out.println(RESET);
+	return Result;
+}
+
+public boolean Get_Response_JSON() {
+	
+	boolean Result = true;
+	
+
+	try {
+		new Thread(new Runnable()
+    {
+                public void run()
+        {
+            {
+                while(true)
+                {		
+                    try {
+                        GENRAL_IS = new DataInputStream(SOCKET.getInputStream());
+                        
+                        
+                        
+                        
+                    
+                        RESPONSE = new JSONObject(GENRAL_IS.readLine());
+                        String statusName = RESPONSE.getJSONObject("requestStatus").getString("statusName");
+                        System.out.println(statusName);
+                        if("SignIn".equalsIgnoreCase(statusName))
+                        {
+                        //ToDO Player object 
+                            Fill_player_Data();
+                            SignIn_Status=true;
+                        }
+                        else if("SignUp".equalsIgnoreCase(statusName))
+                        {
+                        //ToDO Player object 
+                            Fill_player_Data();
+                            System.out.println(getPlayer_Name());
+                            SignUp_Status=true;
+                        }
+                    
+                        
+                    } 
+                    catch (JSONException ex) {
+                        Logger.getLogger(PlayerController.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    catch (IOException ex) {
+                        Logger.getLogger(PlayerController.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+            }
+        }
+    }}).start();
+	} catch (Exception e) {
+		Result = false;
+		System.out.println(NORESET);
+		e.printStackTrace();
+	}
+	System.out.println(RESET);
+	return Result;
+}
+
+public void Fill_player_Data()
+    {
+                        Data_Array= RESPONSE.getJSONArray("Data");
+                        for (int i = 0; i < Data_Array.length(); i++) {
+                           System.out.println("Data   "+Data_Array.getJSONObject(i).getString("Email"));
+                            setPlayer_Email(Data_Array.getJSONObject(i).getString("Email"));
+                            setPlayer_Name(Data_Array.getJSONObject(i).getString("Name"));
+                            setPlayer_Points(Integer.parseInt(Data_Array.getJSONObject(i).getString("Points")));
+                   }
+            System.out.println(getPlayer_Email());
+
+    }
+public boolean CloseConnection() {
+	
+	boolean Result = true;
+	
+		
+	try {
+		
+		GENRAL_IS.close();
+		GENRAL_OS.close();
+			
+	} catch (Exception e) {
+		Result = false;
+		System.out.println(NORESET);
+		e.printStackTrace();
+	}
+	System.out.println(RESET);
+	return Result;
+}
+
 
     public static void main(String[] args)
     {
    PlayerController x= new PlayerController(); 
-    x.SignUpJSON("Mohammed Abouserei","medoaboserii@gmail.com","test123");
+    //x.SignInJSON("medoaboserii@gmail.com","test123");
+   //x.GetUsersJSON();
+   x.Send_GameRequest_JSON("medoaboserii@gmail.com","ahmed@gmail.com");
     }
     
 }
