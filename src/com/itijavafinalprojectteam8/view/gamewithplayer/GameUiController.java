971c6431bf02e2a94ec862969493abbf907166f7
Atef Hares <atefhares1@gmail.com>
2020-01-14 12:13:18 +0200

    merging commit 94204e2d5f72b689be0380dc50a4adfed5dec36a from development_shimaa_2

--- src/com/itijavafinalprojectteam8/view/gamewithplayer/GameUiController.java
+++ src/com/itijavafinalprojectteam8/view/gamewithplayer/GameUiController.java
@@ -1,24 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.itijavafinalprojectteam8.view.gamewithplayer;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import com.itijavafinalprojectteam8.Player;
-import com.itijavafinalprojectteam8.controller.AiLibrary;
 import com.itijavafinalprojectteam8.controller.ClientController;
 import com.itijavafinalprojectteam8.controller.JsonOperations;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
@@ -26,11 +14,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
-import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
